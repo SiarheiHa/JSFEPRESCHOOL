45b22e03b304455bb24ad84b2b34aa831e15924c
SiarheiHa <belposchta@gmail.com>
2022-02-04 11:23:10 +0300

    feat: add audioplayer

--- eco-sounds/index.js
+++ eco-sounds/index.js
@@ -1,122 +1,105 @@
- ///
-const button = document.querySelector('button');
-
+///
+const button = document.querySelector("button");
 const audio = new Audio();
+const birdItem = document.querySelectorAll(".nav-item");
+let isPlay = false;
+const navList = document.querySelector(".nav-list");
+const logo = document.querySelector(".logo");
 
 function playAudio() {
-    if(!isPlay) {
-        // audio.src = './assets/audio/forest.mp3';
-        audio.src = sourceFinder()
-        audio.currentTime = 0;
-        audio.play();
-        isPlay = true
-        // console.log(isPlay)
-    } else {
-        audio.pause();
-        isPlay = false
-        // console.log(isPlay)
-    }
-    toggleButton()
+  audio.src = sourceFinder();
+  audio.currentTime = 0;
+  audio.play();
+  isPlay = true;
 }
 
-const birdItem = document.querySelectorAll('.nav-item')
-
-function sourceFinder () {
-    // const birdItem = document.querySelectorAll('.nav-item')
-    let source = './assets/audio/forest.mp3'
-    console.log(birdItem)
-
-    for(let i = 0; i < birdItem.length; i++) {
-        if (birdItem[i].classList.contains('active')) {
-            const birdName = birdItem[i].dataset.bird
-            source = `./assets/audio/${birdName}.mp3`
-            console.log(source)
-            return source
-        }
-    }
-
-
- 
-    // for (let el in birdItem) {
-    //     console.log(birdItem[el])
-    //     if (birdItem[el].classList.contains('active')) {
-    //         const birdName = birdItem[el].dataset.bird
-    //         source = `./assets/audio/${birdName}.mp3`
-    //         console.log(source)
-    //         return source
-    //     }   
-
-    // }
-
-    return source
+function buttonHandler() {
+  if (!isPlay) {
+    playAudio();
+    addPause();
+  } else {
+    pauseAudio();
+  }
 }
 
-// function pauseAudio() {
-//     audio.pause();
+function pauseAudio() {
+  audio.pause();
+  isPlay = false;
+  removePause();
+}
 
-//     isPlay = false
-//     console.log(isPlay)
-//   }
+function addPause() {
+  button.classList.add("pause");
+}
 
-button.addEventListener('click', playAudio);
-// logo.addEventListener('click', pauseAudio);
+function removePause() {
+  button.classList.remove("pause");
+}
 
-let isPlay = false;
-// console.log(isPlay)
+function sourceFinder() {
+  let source = "./assets/audio/forest.mp3";
+  for (let i = 0; i < birdItem.length; i++) {
+    if (birdItem[i].classList.contains("active")) {
+      const birdName = birdItem[i].dataset.bird;
+      source = `./assets/audio/${birdName}.mp3`;
 
+      return source;
+    }
+  }
+  return source;
+}
 
+function navListHandler(event) {
+  if (event.target.classList.contains("nav-item")) {
+    let clickedItem = event.target;
+    if (!isPlay) {
+      addPause();
+    }
+    removeSelectedItems();
+    selectClickedItem(clickedItem);
+    changeBackground(clickedItem);
+    playAudio();
+  }
+}
+
+function removeSelectedItems() {
+  const items = document.querySelectorAll(".nav-item");
+  items.forEach((el) => el.classList.remove("active"));
+}
+
+function selectClickedItem(clickedItem) {
+  clickedItem.classList.add("active");
+}
+
+function changeBackground(clickedItem) {
+  const main = document.querySelector(".main");
+  const birdName = clickedItem.dataset.bird;
+  main.style.backgroundImage = `url(./assets/img/${birdName}.jpg)`;
+}
+
+function playStartSound(event) {
+  if (!isPlay) {
+    addPause();
+  }
+  removeSelectedItems();
+  playAudio();
+  changeBackground(event.target);
+}
+
+button.addEventListener("click", buttonHandler);
+
+navList.addEventListener("click", navListHandler);
+logo.addEventListener("click", playStartSound);
 
 ///////////////////////////////////////////
 
-function toggleButton() {
-    button.classList.toggle('pause');
-  }
-//   button.addEventListener('click', toggleBtn);
+// for (let el in birdItem) {
+//     console.log(birdItem[el])
+//     if (birdItem[el].classList.contains('active')) {
+//         const birdName = birdItem[el].dataset.bird
+//         source = `./assets/audio/${birdName}.mp3`
+//         console.log(source)
+//         return source
+//     }
 
- ///////////////////////////
-
- const navList = document.querySelector('.nav-list')
-
- function navListHandler (event) {
-     if(event.target.classList.contains('nav-item')) {
-        let clickedItem = event.target
-        // console.log(clickedItem)
-        removeSelectedItems ()
-        selectClickedItem(clickedItem)
-        changeBackground(clickedItem)
-        playAudio ()
-        // changeSound(clickedItem)
-     }
- }
-
-//  function changeSound (clickedItem) {
-//     const birdName = clickedItem.dataset.bird
-//     const source = `./assets/audio/${birdName}.mp3`
-//     playAudio(source)
-//     console.log(source)
-
-//  }
-
- function removeSelectedItems () {
-    const items = document.querySelectorAll('.nav-item')
-    items.forEach((el) => el.classList.remove('active'))
-}
-
- function selectClickedItem (clickedItem) {
-     clickedItem.classList.add('active')
-}
-
-function changeBackground (clickedItem) {
-    const main = document.querySelector('.main')
-    const birdName = clickedItem.dataset.bird
-    main.style.backgroundImage = `url(./assets/img/${birdName}.jpg)`
-
-}
-
-
-
-
-
-
-
- navList.addEventListener('click', navListHandler)
+// }
